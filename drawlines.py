0cb6409a519b1e528ea737f289c2875ff7fc6cd5
Jay Rambhia <jayrambhia777@gmail.com>
2012-03-20 00:31:03 +0545

    added comments.

--- drawlines.py
+++ drawlines.py
@@ -2,8 +2,17 @@
 Author: Jay Rambhia
 email: jayrambhia777@gmail.com
 irc nick: jayrambhia
-blog: jayrambhia.wordpress.com
+blog: http://jayrambhia.wordpress.com
 '''
+"""
+drawlines.
+Draws lines on image joining two distinct point. Points are marked using mouse clicks.
+
+Usage:
+drawlines(img)
+
+credits: SimpleCV library
+"""
 from SimpleCV import *
 
 def drawlines(img):
@@ -15,15 +24,16 @@ def drawlines(img):
     while disp.isNotDone():
         if disp.mouseLeft:
             if pt1 is None and pt2 is None:
-                pt1 = pg.mouse.get_pos()
+                pt1 = pg.mouse.get_pos()    # get first point. point is a tuple of two integers.
             elif pt1:
-                pt2 = pg.mouse.get_pos()
+                pt2 = pg.mouse.get_pos()    # get second point
             
             if pt1 is not None and pt2 is not None:
                 if pt1 != pt2:
-                    img.drawLine(pt1,pt2,thickness=2)
+                    # if points are distinct
+                    img.drawLine(pt1,pt2,thickness=2)   # draw the line joining both the points
                     img.show()
-                    pt1= None
+                    pt1= None  
                     pt2 = None
 
 if __name__ == "__main__":
