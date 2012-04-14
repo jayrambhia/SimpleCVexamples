3696ed6a34d79b34d1b32684ded614e594503624
Jay Rambhia <jayrambhia777@gmail.com>
2012-04-15 01:56:24 +0530

    removed unnecessary stuff

--- pygtk_image_test.py
+++ pygtk_image_test.py
@@ -11,12 +11,6 @@ def loadimage():
     time.sleep(3)
     d.show_image(Image("simplecv").toRGB().getBitmap())
     
-def thread_gtk():
-    Thread(target=start_gtk).start()
-    
-def start_gtk():
-    gtk.main()
-    
 
 if __name__ == "__main__":
     loadimage()
