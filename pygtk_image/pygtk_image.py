80a0438027e3bc4ca0c64d0cd2afb360e59d331e
Jay Rambhia <jayrambhia777@gmail.com>
2012-04-16 18:33:51 +0530

    added more functinalities

--- pygtk_image/pygtk_image.py
+++ pygtk_image/pygtk_image.py
@@ -5,14 +5,12 @@ pygame with pygtk.
 Created a small test for it. pygtk_image_test.py
 """
 import gtk
-from SimpleCV import *
 from threading import Thread
 import gobject
 gtk.gdk.threads_init()
 
 class DisplayImage():
     def __init__(self,title="SimpleCV"):
-        #print "Initialize"
         self.img = None
         self.img_gtk = None
         self.mouseX = 0
@@ -21,12 +19,18 @@ class DisplayImage():
         self.mouse_rawY = 0
         self.done = False
         self.thrd = None
+        self.mouseLeft = 0
+        self.mouseRight = 0
+        self.mouseMiddle = 0
+        self.leftButtonDown = 0
+        self.rightButtonDown = 0
         self.win = gtk.Window()
         self.win.set_title(title)
         self.win.connect("delete_event",self.leave_app)
         self.image_box = gtk.EventBox()
         self.image_box.connect("motion_notify_event",self.motion_callback)
         self.image_box.connect("button_press_event",self.press_callback)
+        self.image_box.connect("button_release_event",self.release_callback)
         self.win.add(self.image_box)
         #self.thread_gtk()           # thread for gtk.main() . Need to 
                                     # consider multiple images too.
@@ -49,36 +53,61 @@ class DisplayImage():
         self.win.show_all()
         if not self.img_flag:
             self.thread_gtk()                   # gtk.main() only once (first time)
-            #print "change flag"
             self.img_flag=1                     # change flag
         
     def thread_gtk(self):
         # changed this function. Improved threading.
-        #print "create thread"
         self.thrd = Thread(target=gtk.main, name = "GTK thread")
         self.thrd.daemon = True
-        #print "start thread"
         self.thrd.start()
-        #print "thread_gtk end"
     
     def leave_app(self,widget,data):
         self.done = True
         self.win.destroy()
         gtk.main_quit()
-        
+    
+    def release_callback(self,widget,event):
+        self.mouseX = int(event.x)
+        self.mouseY = int(event.y)
+        self.mouse_rawX = int(event.x_root)
+        self.mouse_rawY = int(event.y_root)
+        self.setReleaseButtonState(event.button)
+        #print self.mouseX, self.mouseY, self.mouse_rawX, self.mouse_rawY,"leave"
+    
     def press_callback(self,widget,event):
         self.mouseX = int(event.x)
         self.mouseY = int(event.y)
         self.mouse_rawX = int(event.x_root)
         self.mouse_rawY = int(event.y_root)
-        print self.mouseX, self.mouseY, self.mouse_rawX, self.mouse_rawY,"press"
+        self.setPressButtonState(event.button)
+        #print self.mouseX, self.mouseY, self.mouse_rawX, self.mouse_rawY,"press"
+        
+    def setPressButtonState(self, mode):
+        if mode == 1:
+            self.leftButtonDown = 1
+            self.mouseLeft = 1
+        elif mode == 2:
+            self.rightButtonDown = 1
+            self.mouseLeft = 1
+        elif mode == 3:
+            self.mouseMiddle = 1
+            
+    def setReleaseButtonState(self, mode):
+        if mode == 1:
+            self.leftButtonDown = 0
+            self.mouseLeft = 0
+        elif mode == 2:
+            self.rightButtonDown = 0
+            self.mouseLeft = 0
+        elif mode == 3:
+            self.mouseMiddle = 0
         
     def motion_callback(self,widget,event):
         self.mouseX = int(event.x)
         self.mouseY = int(event.y)
         self.mouse_rawX = int(event.x_root)
         self.mouse_rawY = int(event.y_root)
-        print self.mouseX, self.mouseY, self.mouse_rawX, self.mouse_rawY,"motion"
+        #print self.mouseX, self.mouseY, self.mouse_rawX, self.mouse_rawY,"motion"
     
     def isDone(self):
         return self.done
@@ -87,4 +116,3 @@ class DisplayImage():
         self.done = True
         self.win.destroy()
         gtk.main_quit()
-    
